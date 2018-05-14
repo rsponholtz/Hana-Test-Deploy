d47ad70b2f478cc6317d0666b091613231ca5444
dkmsft <dakalmin@microsoft.com>
2018-05-14 23:22:14 +0000

    dk: 314

--- sap-iscsi-server/iscsiserver.sh
+++ sap-iscsi-server/iscsiserver.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+
+IQN1=$1
+IQN1CLT1=$2
+IQN1CLT2=$3
+IQN2=$4
+IQN2CLT1=$5
+IQN2CLT2=$6
+IQN3=$7
+IQN3CLT1=$8
+IQN3CLT2=$9
+
+
 #step 2
 zypper update -y
 #step 3 (with SP3 updates)
@@ -18,28 +30,29 @@ systemctl start targetcli
 #create the first iscsi target
 dd if=/dev/zero of=/iscsi_disks/disk01.img count=0 bs=1 seek=1G 
 targetcli backstores/fileio create cl1 /iscsi_disks/disk01.img 1G
-targetcli iscsi/ create iqn.2006-04.cl1.local:cl1
-targetcli iscsi/iqn.2006-04.cl1.local:cl1/tpg1/luns/ create /backstores/fileio/cl1
-targetcli iscsi/iqn.2006-04.cl1.local:cl1/tpg1/acls/ create iqn.2006-04.prod-cl1-0.local:prod-cl1-0
-targetcli iscsi/iqn.2006-04.cl1.local:cl1/tpg1/acls/ create iqn.2006-04.prod-cl1-1.local:prod-cl1-1
+targetcli iscsi/ create $IQN1
+targetcli iscsi/$IQN1/tpg1/luns/ create /backstores/fileio/cl1
+targetcli iscsi/$IQN1/tpg1/acls/ create $IQN1CLT1
+targetcli iscsi/$IQN1/tpg1/acls/ create $IQN1CLT2
 targetcli saveconfig
 systemctl restart target
 
+#create the third iscsi target
 dd if=/dev/zero of=/iscsi_disks/disk02.img count=0 bs=1 seek=1G 
-targetcli backstores/fileio create cl2 /iscsi_disks/disk01.img 1G
-targetcli iscsi/ create iqn.2006-04.cl2.local:cl2
-targetcli iscsi/iqn.2006-04.cl2.local:cl2/tpg2/luns/ create /backstores/fileio/cl2
-targetcli iscsi/iqn.2006-04.cl2.local:cl2/tpg2/acls/ create iqn.2006-04.prod-cl2-0.local:prod-cl2-0
-targetcli iscsi/iqn.2006-04.cl2.local:cl2/tpg2/acls/ create iqn.2006-04.prod-cl2-1.local:prod-cl2-1
+targetcli backstores/fileio create cl2 /iscsi_disks/disk02.img 1G
+targetcli iscsi/ create $IQN2
+targetcli iscsi/$IQN2/tpg2/luns/ create /backstores/fileio/cl2
+targetcli iscsi/$IQN2/tpg2/acls/ create $IQN2CLT1
+targetcli iscsi/$IQN2/tpg2/acls/ create $IQN2CLT2
 targetcli saveconfig
 systemctl restart target
 
 #create the third iscsi target
 dd if=/dev/zero of=/iscsi_disks/disk03.img count=0 bs=1 seek=1G 
 targetcli backstores/fileio create cl3 /iscsi_disks/disk03.img 1G
-targetcli iscsi/ create iqn.2006-04.cl3.local:cl3
-targetcli iscsi/iqn.2006-04.cl3.local:cl3/tpg3/luns/ create /backstores/fileio/cl3
-targetcli iscsi/iqn.2006-04.cl3.local:cl3/tpg3/acls/ create iqn.2006-04.prod-cl3-0.local:prod-cl3-0
-targetcli iscsi/iqn.2006-04.cl3.local:cl3/tpg3/acls/ create iqn.2006-04.prod-cl3-1.local:prod-cl3-1
+targetcli iscsi/ $IQN3
+targetcli iscsi/$IQN3/tpg3/luns/ create /backstores/fileio/cl3
+targetcli iscsi/$IQN3/tpg3/acls/ create $IQN3CLT1
+targetcli iscsi/$IQN3/tpg3/acls/ create $IQN3CLT2
 targetcli saveconfig
 systemctl restart target
