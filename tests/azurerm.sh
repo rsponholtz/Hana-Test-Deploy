22eefe252429087bcee9a4b9b2c2c2ad0ddcbbbc
dkmsft <dakalmin@microsoft.com>
2018-05-15 12:06:33 +0000

    dk: 315

---
+++ tests/azurerm.sh
@@ -0,0 +1,7 @@
+rgname=azuredeploy-full-3
+vnetname=vnet
+subnetname=subnet
+vmpassword="AweS0me@PW1234" 
+customuri="https://stagea3d54928db62458aad6.blob.core.windows.net/hanarg5-stageartifacts"
+
+az group delete --name $rgname 
