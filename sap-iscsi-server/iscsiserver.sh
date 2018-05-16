91d1c960450d69f8b8661ab72f953a933d6a3fe7
dkmsft <dakalmin@microsoft.com>
2018-05-16 22:32:35 +0000

    dk: 360

--- sap-iscsi-server/iscsiserver.sh
+++ sap-iscsi-server/iscsiserver.sh
@@ -31,9 +31,9 @@ mkdir /iscsi_disks
 
 #create the first iscsi target
 dd if=/dev/zero of=/iscsi_disks/disk01.img count=0 bs=1 seek=1G 
-targetcli backstores/fileio create cl1 /iscsi_disks/disk01.img 1G
+targetcli backstores/fileio create cl1a /iscsi_disks/disk01.img 1G
 targetcli iscsi/ create "$IQN1"
-targetcli iscsi/"$IQN1"/tpg1/luns/ create /backstores/fileio/cl1
+targetcli iscsi/"$IQN1"/tpg1/luns/ create /backstores/fileio/cl1a
 targetcli iscsi/"$IQN1"/tpg1/acls/ create "$IQN1CLT1"
 targetcli iscsi/"$IQN1"/tpg1/acls/ create "$IQN1CLT2"
 targetcli --yes saveconfig
@@ -41,20 +41,20 @@ systemctl restart target
 
 #create the second iscsi target
 dd if=/dev/zero of=/iscsi_disks/disk02.img count=0 bs=1 seek=1G 
-targetcli backstores/fileio create cl2 /iscsi_disks/disk02.img 1G
+targetcli backstores/fileio create cl2a /iscsi_disks/disk02.img 1G
 targetcli iscsi/ create "$IQN2"
-targetcli iscsi/"$IQN2"/tpg2/luns/ create /backstores/fileio/cl2
-targetcli iscsi/"$IQN2"/tpg2/acls/ create "$IQN2CLT1"
-targetcli iscsi/"$IQN2"/tpg2/acls/ create "$IQN2CLT2"
+targetcli iscsi/"$IQN2"/tpg1/luns/ create /backstores/fileio/cl2a
+targetcli iscsi/"$IQN2"/tpg1/acls/ create "$IQN2CLT1"
+targetcli iscsi/"$IQN2"/tpg1/acls/ create "$IQN2CLT2"
 targetcli --yes saveconfig
 systemctl restart target
 
 #create the third iscsi target
 dd if=/dev/zero of=/iscsi_disks/disk03.img count=0 bs=1 seek=1G 
-targetcli backstores/fileio create cl3 /iscsi_disks/disk03.img 1G
-targetcli iscsi/ "$IQN3"
-targetcli iscsi/"$IQN3"/tpg3/luns/ create /backstores/fileio/cl3
-targetcli iscsi/"$IQN3"/tpg3/acls/ create "$IQN3CLT1"
-targetcli iscsi/"$IQN3"/tpg3/acls/ create "$IQN3CLT2"
+targetcli backstores/fileio create cl3a /iscsi_disks/disk03.img 1G
+targetcli iscsi/ create "$IQN3"
+targetcli iscsi/"$IQN3"/tpg1/luns/ create /backstores/fileio/cl3a
+targetcli iscsi/"$IQN3"/tpg1/acls/ create "$IQN3CLT1"
+targetcli iscsi/"$IQN3"/tpg1/acls/ create "$IQN3CLT2"
 targetcli --yes saveconfig
 systemctl restart target
