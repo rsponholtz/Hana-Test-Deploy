7e27de9fbe6c81d64aef15f1980bb1ac5c9798ed
dkmsft <dakalmin@microsoft.com>
2019-02-20 04:22:56 +0000

    dk: 795

--- tests/pas-sw.sh
+++ tests/pas-sw.sh
@@ -14,14 +14,32 @@ az group deployment create \
 --resource-group "$rgname" \
    --template-uri "https://raw.githubusercontent.com/AzureCAT-GSI/Hana-Test-Deploy/master/sap-netweaver-server/azuredeploy-nw-sw.json" \
    --parameters \
-   vmName="$PASVMNAME" \
-   vmUserName="$vmusername" \
-   vmPassword="$vmpassword" \
-   vnetName="$vnetname" \
-   ExistingNetworkResourceGroup="$rgname" \
-   vmSize="Standard_DS2_v2" \
-   osType="SLES 12 SP3" \
-   appAvailSetName="nwavailset" \
-   StaticIP="$PASIPADDR"
+        VMNAME="$PASVMNAME" \
+        VMIPADDR="$PASIPADDR" \
+        ISPRIMARY="YES" \
+        URI="$customuri" \
+        NFSILBIP="$NFSILBIP" \
+        ASCS1VM="$ASCSVMNAME1" \
+        ASCS1IP="$ASCSIP1" \
+        ASCS2VM="$ASCSVMNAME2" \
+        ASCS2IP="$ASCSIP2" \
+        MASTERPASSWORD="$vmpassword" \
+        DBHOST="hanailb" \
+        DBSID="$HANASID" \
+        DBINSTANCE="00" \
+        ASCSSID="$ASCSSID" \
+        ASCSHOST="ascsilb" \
+        NWINSTANCE="05" \
+        ASCSINSTANCE="00" \
+        ERSINSTANCE="01" \
+        SAPBITSMOUNT="$SAPBITSMOUNT" \
+        SAPMNTMOUNT="$SAPMNTMOUNT" \
+        USRSAPSIDMOUNT="$USRSAPSIDMOUNT" \
+        USRSAPASCSMOUNT="$USRSAPASCSMOUNT" \
+        USRSAPERSMOUNT="$USRSAPERSMOUNT" \
+        ASCSILBIP="$ASCSLBIP" \
+        DBIP="$HANAILBIP" 
 
 echo "netweaver cluster created"
+
+
