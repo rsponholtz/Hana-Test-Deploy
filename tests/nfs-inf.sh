674e8083ace3a42d3d9fd1acf2ee124cb8d9d05a
Ross Sponholtz <rsponholtz@hotmail.com>
2020-05-06 14:05:51 -0700

    update image specs

--- tests/nfs-inf.sh
+++ tests/nfs-inf.sh
@@ -22,7 +22,7 @@ az group deployment create \
    SubnetName=$appsubnetname \
    VMUserName=$vmusername \
    VMPassword=$vmpassword \
-   OperatingSystem="SLES 15 SP1" \
+   OperatingSystem="SLES for SAP 15 SP1" \
    ExistingNetworkResourceGroup=$vnetrgname \
    StaticIP1=$NFSIP1 \
    StaticIP2=$NFSIP2 \
